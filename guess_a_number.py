64ad698d2b1ce2e1cdb1478db862e518d4e8a4a7
Bergquist <sbergq7071@greenville.k12.sc.us>
2017-09-18 09:10:20 -0400

    Made it yes or no

--- guess_a_number.py
+++ guess_a_number.py
@@ -6,12 +6,6 @@ low = 1
 high = 100000
 limit = 10
 
-rand = random.randint(low, high)
-print("I'm thinking of a number from " + str(low) + " to " + str(high) + ".");
-
-guess = -1, 
-tries = 0
-
 #helper functions
 def get_guess():
     while True:
@@ -22,22 +16,46 @@ def get_guess():
             return g
         else:
             print("You must enter a number")
+            
+def play_again():
+    while True:
+        decision = input("Would you like to play again? (y/n) ")
+
+        if decision == "y" or decision == "yes":
+            return True
+        elif decision == "n" or decision == "no":
+            return False
+
+        print("I don't understand. Answer 'y' or 'n'.")
         
-#play the game nerd
-while guess != rand and tries < limit:
-    guess = input("Take a guess: ")
-    guess = int(guess)
-    
-    if guess < rand:
-        print("You guessed too low.")
-    elif guess > rand:
-        print("You guessed too high.")
 
-    tries += 1
+again = True
 
-#tell the player the outcome
-    
-if guess == rand:
-    print("Good job buddy.")
-else:
-    print("You are a brick. The number was " + str(rand) + ".")
+while again:
+    #start game
+    rand = random.randint(low, high)
+    print("I'm thinking of a number from " + str(low) + " to " + str(high) + ".");
+
+    guess = -1, 
+    tries = 0
+            
+    #play the game nerd
+    while guess != rand and tries < limit:
+        guess = input("Take a guess: ")
+        guess = int(guess)
+        
+        if guess < rand:
+            print("You guessed too low.")
+        elif guess > rand:
+            print("You guessed too high.")
+
+        tries += 1
+
+    #tell the player the outcome
+        
+    if guess == rand:
+        print("Good job buddy.")
+    else:
+        print("You are a brick. The number was " + str(rand) + ".")
+
+    again = play_again()
